7d17d2b2b4c0d95f29cf4fc053c3389a2a62c586
RoyiJourno <royijourno@gmail.com>
2018-06-09 16:28:23 +0300

    יש התחלה:)

--- client/myshoppinglist/App.js
+++ client/myshoppinglist/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Hello Royi</Text>
+        <Button title="Create New List"  buttonStyle={styles.buttonStyle}/>
+        <Button title="Show List" buttonStyle={styles.buttonStyle}/>
+        <Button title="Update Tamplate list" buttonStyle={styles.buttonStyle}/>
       </View>
     );
   }
@@ -13,9 +17,24 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    },
+  textStyle:{
+    fontSize:30,
+    fontWeight:'bold',
+    marginBottom:100,
+    },
+  buttonStyle: {
+    backgroundColor: 'rgb(196, 4, 37)',
+    borderRadius: 5,
+    width: 170,
+    marginBottom:20,
   },
+  buttonTitleStyle:{
+    fontWeight:'700',
+    fontSize:24,
+  }
 });
