b803ec7007891f9e32ae2eb07c9021624f40ebe4
razmishor <razmishor@gmail.com>
2018-06-19 17:22:04 +0300

    harta barta

--- client/myshoppinglist/App.js
+++ client/myshoppinglist/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
+  
+  onPress = () =>{
+    alert('raz');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.textStyle}>Hello Royi</Text>
-        <Button title="Create New List"  buttonStyle={styles.buttonStyle}/>
-        <Button title="Show List" buttonStyle={styles.buttonStyle}/>
-        <Button title="Update Tamplate list" buttonStyle={styles.buttonStyle}/>
+        <Text style={styles.textStyle}>Hello Royi34</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onPress}
+        >
+          <Text style = {styles.textStyle}>TouchbleOpacity_Btn</Text>
+        </TouchableOpacity>
+        <Button title="Create New List" buttonStyle={styles.buttonStyle} />
+        <Button title="Show List" buttonStyle={styles.buttonStyle} />
+        <Button title="Update Tamplate list" buttonStyle={styles.buttonStyle} />
       </View>
     );
   }
@@ -17,24 +28,30 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    },
-  textStyle:{
-    fontSize:30,
-    fontWeight:'bold',
-    marginBottom:100,
-    },
+  },
+  textStyle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingBottom: 100,
+  },
   buttonStyle: {
     backgroundColor: 'rgb(196, 4, 37)',
     borderRadius: 5,
     width: 170,
-    marginBottom:20,
+    marginBottom: 20,
   },
-  buttonTitleStyle:{
-    fontWeight:'700',
-    fontSize:24,
+  buttonTitleStyle: {
+    fontWeight: '700',
+    fontSize: 24,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    padding: 10,
   }
 });
